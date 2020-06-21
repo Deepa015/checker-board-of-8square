b4980495ef0ac5485a1f0fabf14befcfa3356a50
Deepa015 <65914574+Deepa015@users.noreply.github.com>
2020-06-21 10:39:52 +0530

    Add files via upload

---
+++ checker board 8 square.py
@@ -0,0 +1,18 @@
+Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 23:11:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======================================= RESTART: C:/Users/DEEKSHA D SALVANKAR/AppData/Local/Programs/Python/Python38/cheacker.py ======================================
+Traceback (most recent call last):
+  File "C:/Users/DEEKSHA D SALVANKAR/AppData/Local/Programs/Python/Python38/cheacker.py", line 4, in <module>
+    for i in range(62.5,500,62.5*2):
+TypeError: 'float' object cannot be interpreted as an integer
+>>> 
+======================================= RESTART: C:/Users/DEEKSHA D SALVANKAR/AppData/Local/Programs/Python/Python38/cheacker.py ======================================
+Traceback (most recent call last):
+  File "C:/Users/DEEKSHA D SALVANKAR/AppData/Local/Programs/Python/Python38/cheacker.py", line 4, in <module>
+    for i in range(80.5,640,80.5*2):
+TypeError: 'float' object cannot be interpreted as an integer
+>>> 
+======================================= RESTART: C:/Users/DEEKSHA D SALVANKAR/AppData/Local/Programs/Python/Python38/cheacker.py ======================================
+
+======================================= RESTART: C:/Users/DEEKSHA D SALVANKAR/AppData/Local/Programs/Python/Python38/cheacker.py ======================================
